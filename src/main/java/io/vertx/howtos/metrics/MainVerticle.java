01a39fbc1b042e07936c3342c86129a2afcadf56
Thomas Segismont <tsegismo@redhat.com>
2021-01-20 21:52:39 +0100

    Initial version of the documentation

--- src/main/java/io/vertx/howtos/metrics/MainVerticle.java
+++ src/main/java/io/vertx/howtos/metrics/MainVerticle.java
@@ -1,5 +1,9 @@
 package io.vertx.howtos.metrics;
 
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
@@ -8,20 +12,24 @@ import io.vertx.ext.web.Router;
 import io.vertx.micrometer.MicrometerMetricsOptions;
 import io.vertx.micrometer.PrometheusScrapingHandler;
 import io.vertx.micrometer.VertxPrometheusOptions;
+import io.vertx.micrometer.backends.BackendRegistries;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MainVerticle extends AbstractVerticle {
 
+  // tag::greetings[]
   private static final String[] GREETINGS = {
     "Hello world!",
     "Bonjour monde!",
     "Hallo Welt!",
     "Hola Mundo!"
   };
+  // end::greetings[]
 
   @Override
   public void start() {
+    // tag::consumer[]
     vertx.eventBus().consumer("greetings", msg -> {
       // Simulate processing time between 20ms and 100ms
       long delay = ThreadLocalRandom.current().nextLong(80) + 20L;
@@ -31,9 +39,12 @@ public class MainVerticle extends AbstractVerticle {
         msg.reply(greeting);
       });
     });
+    // end::consumer[]
 
+    // tag::router[]
     Router router = Router.router(vertx);
-    router.route("/metrics").handler(PrometheusScrapingHandler.create());
+    // end::router[]
+    // tag::handler[]
     router.get("/greeting").handler(rc -> {
       vertx.eventBus().<String>request("greetings", null, ar -> {
         if (ar.succeeded()) {
@@ -45,10 +56,16 @@ public class MainVerticle extends AbstractVerticle {
         }
       });
     });
+    // end::handler[]
+    // tag::scraping[]
+    router.route("/metrics").handler(PrometheusScrapingHandler.create());
+    // end::scraping[]
 
+    // tag::http[]
     vertx.createHttpServer()
       .requestHandler(router)
       .listen(8080);
+    // end::http[]
   }
 
   // tag::main[]
@@ -59,6 +76,19 @@ public class MainVerticle extends AbstractVerticle {
     VertxOptions vertxOptions = new VertxOptions()
       .setMetricsOptions(metricsOptions);
     Vertx vertx = Vertx.vertx(vertxOptions);
+
+    PrometheusMeterRegistry registry = (PrometheusMeterRegistry) BackendRegistries.getDefaultNow();
+    registry.config().meterFilter(
+      new MeterFilter() {
+        @Override
+        public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
+          return DistributionStatisticConfig.builder()
+            .percentilesHistogram(true)
+            .build()
+            .merge(config);
+        }
+      });
+
     vertx.deployVerticle(new MainVerticle());
   }
   // end::main[]
